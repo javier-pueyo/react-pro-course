0f7012f81b90ece969da6e7834dd6de7ba225c58
javier-pueyo <javierpueyomir@gmail.com>
2022-12-24 15:11:40 +0100

    rama forms inicial - estilos agregados

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -16,26 +16,25 @@ export const Navigation = () => {
             <div className="main-layout">
                 <nav>
                     <img src={logo} alt="React Logo" />
+                    <ul>
+                        <li>
+                            <NavLink
+                                to="/"
+                                className={({ isActive }) =>
+                                    isActive ? "nav-active" : ""
+                                }
+                            >
+                                Home
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/about">About</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/user">User</NavLink>
+                        </li>
+                    </ul>
                 </nav>
-
-                <ul>
-                    <li>
-                        <NavLink
-                            to="/"
-                            className={({ isActive }) =>
-                                isActive ? "nav-active" : ""
-                            }
-                        >
-                            Home
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/about">About</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/user">User</NavLink>
-                    </li>
-                </ul>
 
                 <Routes>
                     <Route
